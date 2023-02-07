a5ab5ccc3c4081f80beb0dc8325aa9f3e49afec3
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-02-07 20:21:12 -0300

    feat: added component Loading

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -2,5 +2,15 @@ import styled from 'styled-components/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const Container = styled(SafeAreaView)`
-  
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_100};
+  padding: 24px;
+
+  flex-direction: column;
+  justify-content: center;
 `;
+
+export const LoadingIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({ 
+  color: theme.COLORS.GREEN_DARK,
+  size: theme.FONT_SIZE.XXL 
+}))``;
