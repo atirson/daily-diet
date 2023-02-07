fbc22b5f4864700214147f41ccabbfc1a0abda20
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-02-07 20:03:29 -0300

    configs: added libs (Styled Components, Icons, Fonts and etc)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme'
 
 export default function App() {
+  const [ fontLoader ] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  })
+
   return (
-    <View style={styles.container}>
+    <ThemeProvider theme={theme}>
+      <StatusBar style="dark" translucent />
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,28 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+       [
+        "module-resolver",
+        {
+          "root": ["./src"],
+          "alias": {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@assets": "./src/assets",
+            "@routes": "./src/routes",
+            "@hooks": "./src/hooks",
+            "@context": "./src/context",
+            "@services": "./src/services",
+            "@config": "./src/config",
+            "@constants": "./src/constants",
+            "@styles": "./src/styles",
+            "@types": "./src/types",
+            "@storage": "./src/storage",
+          }
+       }
+      ]
+    ]
   };
 };
