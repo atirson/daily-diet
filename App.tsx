6b71e42b65a1f185396a7e4a28f19f67ced8e2e5
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-02-07 20:20:51 -0300

    config: added routes element

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
+import { Loading } from '@components/Loading';
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+import { Routes } from '@routes/index';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
 import theme from './src/theme'
@@ -14,7 +15,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="dark" translucent />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {!fontLoader ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
